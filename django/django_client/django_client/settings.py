945ba0c44071cc5d1de7aa5f4a8ab09f4162511d
David Alvarez <david@southfloridaathleticclub.com>
2022-05-08 15:01:37 -0400

    Some changes

--- django/django_client/django_client/settings.py
+++ django/django_client/django_client/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import json
-
-with open("./config.json") as config_file:
-    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = os.environ.get['DJANGO_SECRET_KEY']
 
 # Environment Variables
 domain = os.environ.get('DOMAIN')
